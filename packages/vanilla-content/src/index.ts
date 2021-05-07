09694c5869453f02efd20fef53062c887c1fc33e
湫曗 <autumnsilverleaves@gmail.com>
2021-05-07 23:32:47 +0800

    add windicss and stylus

--- packages/vanilla-content/src/index.ts
+++ packages/vanilla-content/src/index.ts
@@ -1,6 +1,15 @@
+import "./index.styl"
+import 'virtual:windi.css'
 const container = document.querySelector("#app")
 const editor = document.createElement("div")
 editor.contentEditable="true"
-editor.classList.add("deal-empty")
 editor.classList.add("shadow")
+editor.classList.add("outline-none")
+editor.classList.add("m-4")
+editor.classList.add("p-4")
+const span = document.createElement("span")
+span.innerText="你好世界"
+editor.append(span)
+span.classList.add("deal-empty")
+
 container.appendChild(editor)
